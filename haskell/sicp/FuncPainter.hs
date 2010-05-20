629ecabd381f884978310820555a0dc22665dcea
HIBINO Kei <hibi@utmc.or.jp>
2010-05-20 16:25:48 +0900

    Implement a method of FuncP.

--- haskell/sicp/FuncPainter.hs
+++ haskell/sicp/FuncPainter.hs
@@ -25,7 +25,24 @@ segments2painter segList frame =
   segList
     where frameMap = frameCoordMap frame
 
+type FPainter f vec num = f (vec num) -> ()
+data FuncP f vec num = FuncP (FPainter f vec num)
 
-data FuncP f vec num = FuncP (f (vec num) -> ())
+transformPainter :: (Frame f, Num num, Vect vec) =>
+                    (FPainter f vec num) -> vec num -> vec num -> vec num -> FPainter f vec num
+transformPainter painter origin corner1 corner2 frame = v
+  where v = painter (makeFrame
+                     newOrigin
+                     (subVect (m corner1) newOrigin)
+                     (subVect (m corner2) newOrigin))
+        m = frameCoordMap frame
+        newOrigin = m origin
+        
 
-instance (Frame f, Num num, Vect vec) => Painter (FuncP f vec num) where
+instance (Frame f, Fractional num, Vect vec) => Painter (FuncP f vec num) where
+  
+  beside (FuncP painter1) (FuncP painter2) = FuncP f
+    where splitPoint = (makeVect 0.5 0.0)
+          paintLeft  = transformPainter painter1 (makeVect 0.0 0.0) splitPoint (makeVect 0.0 1.0)
+          paintRight = transformPainter painter2 splitPoint (makeVect 1.0 0.0) (makeVect 0.5 1.0)
+          f frame = foldl' (\() () -> ()) () [paintLeft frame, paintRight frame]
