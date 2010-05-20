56c156aaf7ea240fb27173d1baa292ff5a316c48
HIBINO Kei <hibi@utmc.or.jp>
2010-05-20 13:33:14 +0900

    Wave painter vertex list.

--- haskell/sicp/Q2_49.hs
+++ haskell/sicp/Q2_49.hs
@@ -34,6 +34,40 @@ painterC = segments2painter $ map seg [0, 1, 2, 3]
   where mid n = let (v1, v2) = vecPairN n in scaleVect (1 / 2) $ addVect v1 v2
         seg n = makeSegment (mid n) $ mid $ (+) n 1
 
+-- left top
+linesLT = [makeVect 0.4  1.0,
+           makeVect 0.35 0.8,
+           makeVect 0.4  0.6,
+           makeVect 0.3  0.6,
+           makeVect 0.2  0.55,
+           makeVect 0.0  0.8]
+
+-- left bottom
+linesLB = [makeVect 0.0  0.6,
+           makeVect 0.2  0.4,
+           makeVect 0.3  0.55,
+           makeVect 0.35 0.5,
+           makeVect 0.25 0.0]
+
+-- bottom
+linesB  = [makeVect 0.4 0.0,
+           makeVect 0.5 0.2,
+           makeVect 0.6 0.0]
+
+-- right bottom
+linesRB = [makeVect 0.75 0.0,
+           makeVect 0.65 0.5,
+           makeVect 1.0  0.2]
+
+-- right top
+linesRT = [makeVect 1.0  0.4,
+           makeVect 0.7  0.6,
+           makeVect 0.6  0.6,
+           makeVect 0.65 0.8,
+           makeVect 0.6  1.0]
+          
+linesLT, linesLB, linesB, linesRB, linesRT :: (Fractional num, Vect vec) => [vec num]
+
 painterD = undefined
 
 painterA, painterB, painterC, painterD :: (Frame f, Fractional num, Vect vec) => f (vec num) -> ()
