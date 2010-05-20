322bffe562a0a535ec76e5c6db1799b291715b37
HIBINO Kei <hibi@utmc.or.jp>
2010-05-20 15:03:06 +0900

    Uncurrying.

--- haskell/sicp/Q2_49.hs
+++ haskell/sicp/Q2_49.hs
@@ -40,36 +40,36 @@ painterC = segments2painter $ map seg [0, 1, 2, 3]
         seg n = makeSegment (mid n) $ mid $ (+) n 1
 
 -- left top
-linesLT = [makeVect 0.4  1.0,
-           makeVect 0.35 0.8,
-           makeVect 0.4  0.6,
-           makeVect 0.3  0.6,
-           makeVect 0.2  0.55,
-           makeVect 0.0  0.8]
+linesLT = vectList [(0.4,  1.0),
+                    (0.35, 0.8),
+                    (0.4,  0.6),
+                    (0.3,  0.6),
+                    (0.2,  0.55),
+                    (0.0,  0.8)]
 
 -- left bottom
-linesLB = [makeVect 0.0  0.6,
-           makeVect 0.2  0.4,
-           makeVect 0.3  0.55,
-           makeVect 0.35 0.5,
-           makeVect 0.25 0.0]
+linesLB = vectList [(0.0,  0.6),
+                    (0.2,  0.4),
+                    (0.3,  0.55),
+                    (0.35, 0.5),
+                    (0.25, 0.0)]
 
 -- bottom
-linesB  = [makeVect 0.4 0.0,
-           makeVect 0.5 0.2,
-           makeVect 0.6 0.0]
+linesB  = vectList [(0.4, 0.0),
+                    (0.5, 0.2),
+                    (0.6, 0.0)]
 
 -- right bottom
-linesRB = [makeVect 0.75 0.0,
-           makeVect 0.65 0.5,
-           makeVect 1.0  0.2]
+linesRB = vectList [(0.75, 0.0),
+                    (0.65, 0.5),
+                    (1.0,  0.2)]
 
 -- right top
-linesRT = [makeVect 1.0  0.4,
-           makeVect 0.7  0.6,
-           makeVect 0.6  0.6,
-           makeVect 0.65 0.8,
-           makeVect 0.6  1.0]
+linesRT = vectList [(1.0,  0.4),
+                    (0.7,  0.6),
+                    (0.6,  0.6),
+                    (0.65, 0.8),
+                    (0.6,  1.0)]
           
 linesLT, linesLB, linesB, linesRB, linesRT :: (Fractional num, Vect vec) => [vec num]
 
