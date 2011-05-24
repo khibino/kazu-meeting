8902256e1673ff535279b1febe6a8da3d060776d
HIBINO Kei <hibi@utmc.or.jp>
2011-05-24 23:20:18 +0900

    More strict import.

--- haskell/sicp/Circuit2.hs
+++ haskell/sicp/Circuit2.hs
@@ -3,12 +3,12 @@
 module Circuit2 where
 
 import Prelude hiding((.), id)
-import Control.Category as C
-import Control.Arrow    as A
-import Control.Arrow.Transformer
-import Control.Arrow.Operations
-import Control.Arrow.Transformer.Automaton
---import qualified Data.Stream as S
+import Control.Category ((.), id)
+import Control.Arrow (ArrowLoop, arr, (>>>), (<<<), (&&&))
+import Control.Arrow.Transformer (lift)
+import Control.Arrow.Operations (ArrowCircuit, delay)
+import Control.Arrow.Transformer.Automaton (Automaton, runAutomaton)
+import qualified Data.Stream as S
 
 counter :: ArrowCircuit a => a Bool Int
 counter =  proc reset -> do
@@ -32,10 +32,8 @@ not' :: Auto Bool Bool
 not' =  proc input ->
           nand -< (input, input)
 
-and'' :: Auto (Bool, Bool) Bool
-and'' =  nand >>> not' --not' . nand
-
 and' :: Auto (Bool, Bool) Bool
+--and'' =  nand >>> not' --not' . nand
 and' =  proc (i0, i1) -> do
           m0 <- nand -< (i0, i1)
           not' -< m0
@@ -46,10 +44,8 @@ or' =  proc (i0, i1) -> do
          m1 <- not' -< i1
          nand -< (m0, m1)
 
-xor' :: Auto (Bool, Bool) Bool
-xor' =  (nand &&& or') >>> and'
-
 xor :: Auto (Bool, Bool) Bool
+--xor =  (nand' &&& or') >>> and'
 xor =  proc i -> do
          m0 <- nand -< i 
          m1 <- or'  -< i
@@ -59,6 +55,7 @@ hAdder' :: Auto (Bool, Bool) (Bool, Bool)
 hAdder' =  and' &&& xor
 
 hAdder :: Auto (Bool, Bool) (Bool, Bool)
+--hAdder =  and' &&& xor
 hAdder =  
   proc i -> do
     o0 <- xor  -< i
@@ -73,22 +70,58 @@ adder =
     o1 <- or' -< (m1, m2)
     id -< (o1, o0)
 
+
+-- 遅延あり
+off :: Auto Bool Bool
+off = delay False
+
+not'' :: Auto Bool Bool
+not'' =  proc input ->
+           off <<< off <<< not' -< input
+
+and'' :: Auto (Bool, Bool) Bool
+and'' =  proc (i0, i1) ->
+          off <<< off <<< off <<< and' -< (i0, i1)
+
+or'' :: Auto (Bool, Bool) Bool
+or'' =  proc (i0, i1) ->
+          off <<< off <<< off <<< off <<< off <<< or' -< (i0, i1)
+
+hAdder'' :: Auto (Bool, Bool) (Bool, Bool)
+hAdder'' =
+  proc (a, b) -> do
+    d <- or''  -< (a, b)
+    c <- and'' -< (a, b)
+    e <- not'' -< c
+    s <- and'' -< (d, e)
+    id -< (s, c)
+
+
+runAuto :: Auto b c -> [b] -> [c]
+runAuto a i =
+  S.toList $ runAutomaton (arr snd >>> a) ((), S.fromList i)
+
+-- Tools for test
 tuples2bit :: [(Bool, Bool)]
 tuples2bit =  let b = [False,True] in [(x,y) | x <- b, y <- b ]
 
---test2bit  :: Auto (Bool, Bool) c -> S.Stream c
---test2bit c = runAutomaton c $ ((), S.fromList tuples2bit)
-
---runAuto :: (ArrowLoop a, ArrowApply a) =>
---	Automaton (->) b c -> (->) ((), S.Stream b) (S.Stream c)
--- runAuto :: Automaton (->) b c -> (->) ((), S.Stream b) (S.Stream c)
--- runAuto (Automaton f) = runAutomaton (Automaton (\ ((), v) -> f v))
-
--- test2bit  :: Auto (Bool, Bool) c -> S.Stream c
--- test2bit c = runAuto c $ S.fromList tuples2bit
+test2bit  :: Auto (Bool, Bool) c -> [c]
+test2bit c = runAuto c tuples2bit
 
 tuples3bit :: [((Bool, Bool), Bool)]
 tuples3bit =  let b = [False,True] in [((x,y), z) | x <- b, y <- b, z <- b ]
 
--- test3bit  :: Auto ((Bool, Bool), Bool) c -> S.Stream c
--- test3bit c = runAutomaton c $ S.fromList tuples3bit
+test3bit  :: Auto ((Bool, Bool), Bool) c -> [c]
+test3bit c = runAuto c tuples3bit
+
+sicpInput :: [(Bool, Bool)]
+sicpInput =  replicate 8 (True, False) ++ replicate 10 (True, True)
+
+testSicp :: [(Bool, Bool)]
+testSicp =  runAuto hAdder'' sicpInput
+
+testSicpPick :: IO ()
+testSicpPick =  putStr $ unlines [pick 8, pick 11, pick 16]
+  where probe n = (n, testSicp !! n)
+        pick  n = let m = n - 1 in show (probe m) ++ " --> " ++ show (probe n)
+
