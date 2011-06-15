e7f243bbb20ff1526d20de241c35b4e199b718b5
HIBINO Kei <hibi@utmc.or.jp>
2011-06-15 22:45:25 +0900

    Fix to use PNum.

--- haskell/lazys/EvalTest.hs
+++ haskell/lazys/EvalTest.hs
@@ -1,21 +1,24 @@
 module EvalTest where
 
-import Syntax
+import PrimNum (PNum)
+import Syntax (Literal'(..), Pat(..),
+               Exp, Exp'(..), Bind'(..),
+               Module, Module'(..))
 import Evaluator (evalExp, Result, run)
 
-exp0 :: Exp' Integer
+exp0 :: Exp
 exp0 =  Let [BPat (PVar "x") (Lit (Num 1))] (EVar "x")
 
-test0 :: Result Integer
+test0 :: Result PNum
 test0 =  evalExp exp0
 
-mod1 :: Module' Integer
+mod1 :: Module
 mod1 =  Module "Main" [BPat
                        (PVar "main")
                        (Let [BPat (PVar "x") (Lit (Num 1))] (EVar "x"))]
 
-test1 :: Result Integer
-test1 = run [] mod1
+test1 :: Result PNum
+test1 =  run [] mod1
 
 --exp2 :: 
 --ext2 =  
