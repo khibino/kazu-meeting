230d554d7b456acf0c05285158bb82c87683841d
HIBINO Kei <hibi@utmc.or.jp>
2011-06-15 23:55:47 +0900

    Rename parser functions.

--- haskell/lazys/Parser.hs
+++ haskell/lazys/Parser.hs
@@ -1,6 +1,6 @@
 module Parser (
-  toExpr,
-  toModule
+  parseExpr,
+  parseModule
   ) where
 
 import Control.Applicative ((<$>), (<*>))
@@ -24,26 +24,26 @@ type Parser e = SExp -> ParseResult e
 many :: Parser a -> Parser [a]
 many p = (>>= mapM p) . SExp.toList
 
-toPat :: Parser Pat
-toPat =  match
+parsePat :: Parser Pat
+parsePat =  match
   where match (SExp.Atom (SExp.Id var)) = successResult $ PVar var
         match  form                = parseError "pattern" form
 
-toLambdaParams :: Parser ([Pat], Maybe Var)
-toLambdaParams sExp =
+parseLambdaParams :: Parser ([Pat], Maybe Var)
+parseLambdaParams sExp =
   let (pats, varArg) = SExp.toList1 sExp in
   (,)
-  <$> mapM toPat pats
-  <*> maybe (successResult Nothing) (fmap (Just . runPVar) . toPat) varArg
+  <$> mapM parsePat pats
+  <*> maybe (successResult Nothing) (fmap (Just . runPVar) . parsePat) varArg
 
-toLambda :: Parser Lambda
-toLambda =  match
+parseLambda :: Parser Lambda
+parseLambda =  match
   where match (params' :! expr :! SExp.Nil) =
-          uncurry lambda <$> toLambdaParams params' <*> toExpr expr
+          uncurry lambda <$> parseLambdaParams params' <*> parseExpr expr
         match  form = parseError "lambda" form
 
-toExpr :: Parser Exp
-toExpr =  rec
+parseExpr :: Parser Exp
+parseExpr =  rec
   where atom (SExp.Num n)  = number n
         atom (SExp.Str s)  = string s
         atom (SExp.Id id') = Syntax.EVar id'
@@ -51,34 +51,34 @@ toExpr =  rec
         rec (SExp.Atom a) = successResult $ atom a
         rec form@(SExp.Atom (SExp.Id "let") :! rest)
           | (binds' :! expr :! SExp.Nil) <- rest = Syntax.Let
-                                                   <$> many toBind binds'
+                                                   <$> many parseBind binds'
                                                    <*> rec expr
           | otherwise                     = parseError "let" form
         rec (SExp.Atom (SExp.Id "quote") :! rest) = successResult $ quote rest
         rec (SExp.Atom (SExp.Id sym) :! rest)
-          | sym `elem` ["lambda", "\\"] = Syntax.Abs <$> toLambda rest
+          | sym `elem` ["lambda", "\\"] = Syntax.Abs <$> parseLambda rest
         rec form@(proc :! SExp.Nil) | SExp.functionValue proc = Syntax.VCall <$> rec proc
                                     | otherwise               = parseError "varargs function call expression" form
-        rec form@(proc :! args) | SExp.functionValue proc = Syntax.FApp <$> rec proc <*> many toExpr args
+        rec form@(proc :! args) | SExp.functionValue proc = Syntax.FApp <$> rec proc <*> many parseExpr args
                                 | otherwise               = parseError "function call expression" form
 
 
-toBind :: Parser Bind
-toBind =  dispatch
+parseBind :: Parser Bind
+parseBind =  dispatch
   where dispatch (pat@(SExp.Atom (SExp.Id _)) :! expr :! SExp.Nil) =
-          Syntax.BPat <$> toPat pat <*> toExpr expr
+          Syntax.BPat <$> parsePat pat <*> parseExpr expr
         dispatch ((SExp.Atom (SExp.Id var) :! params') :! body') =
-          Syntax.BFun var <$> toLambda (params' :! body')
+          Syntax.BFun var <$> parseLambda (params' :! body')
         dispatch  form = parseError "bind" form
 
-toModule :: [SExp] -> ParseResult Module
-toModule ees@(e:es) = Module <$> modDecl e <*> topBinds es <|>
+parseModule :: [SExp] -> ParseResult Module
+parseModule ees@(e:es) = Module <$> modDecl e <*> topBinds es <|>
                        Module "Main" <$> topBinds ees
   where modDecl (SExp.Atom (SExp.Id "module")
                  :! SExp.Atom (SExp.Id name') :! SExp.Nil) =
           successResult name'
         modDecl  form = parseError "module declaration" form
         topBinds = mapM bindTop
-        bindTop (SExp.Atom (SExp.Id "define") :! bind) = toBind bind
+        bindTop (SExp.Atom (SExp.Id "define") :! bind) = parseBind bind
         bindTop  form = parseError "bind at top" form
-toModule []         = parseError "module" SExp.Nil
+parseModule []         = parseError "module" SExp.Nil
